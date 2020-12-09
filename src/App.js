fb338747529e089bb0e4df953a5a88aa4b37b22e
Aamir Abbas <aamyear@gmail.com>
2020-12-09 06:18:45 +0400

    Working draft - No styling - Input field has event handler for typing - Flags object is imported from helper - Check if flag is available as a key - Display country name (saved as key value pair on flag object)

--- src/App.js
+++ src/App.js
@@ -1,17 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import helper from "./helper";
 import "./styles.css";
 
 export default function App() {
-  helper.helper1();
-  helper.helper2();
-  helper.helper3();
+  const [msg, setMsg] = useState("");
+
+  // get flag list from helper
+  let flags = helper.setFlagsDB();
+
+  // handle user input
+  function handleUserInput(e) {
+    let userInputFlag = e.target.value.trim();
+    let country = flags[userInputFlag];
+    let dispMsg = "";
+
+    if (country) {
+      // console.log("Exists", country);
+
+      dispMsg = userInputFlag + " is the flag of " + country;
+      // console.log(dispMsg);
+    } else {
+      dispMsg = "Seems like that is not a country flag. ";
+    }
+
+    setMsg(dispMsg);
+  }
+
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h1>Flag Detector!</h1>
+      <h2>
+        Paste the Emoji Flag of your country and we'll tell you where you are
+        from...
+      </h2>
 
-      <input />
+      <input onKeyUpCapture={handleUserInput} />
+      <p>{msg}</p>
     </div>
   );
 }
